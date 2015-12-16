53ccec97a13193fc8a5992ceca60e6b47e3652df
David Julião <djuliao08@gmail.com>
2015-12-16 17:12:02 +0000

    Sobre concluído

---
+++ app/src/main/java/isec/memorygame/SobreActivity.java
@@ -0,0 +1,16 @@
+package isec.memorygame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class SobreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+        ImageView img = (ImageView) findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.isec);
+    }
+}
